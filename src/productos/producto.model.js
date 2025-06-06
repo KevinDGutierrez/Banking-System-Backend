03c9aa4eea5ac7d5d6e030ec76f61c1d85374d89
AlexxoK <dmonterroso-2023234@kinal.edu.gt>
2025-06-06 14:43:06 -0600

    Se agregó existencias al modelo, para manejar el canjeo

--- src/productos/producto.model.js
+++ src/productos/producto.model.js
@@ -13,11 +13,19 @@ const ProductoSchema = Schema({
         maxLength: [5000, "El máximo de carácteres es 5,000!"],
     },
 
+    existencias: {
+        type: Number,
+        required: [true, "La cantidad de existencias es requerida!"],
+        max: [100, "El máximo de existentes es 100!"],
+        min: [1, "El mínimo de existentes es 1!"],
+        default: 1
+    },
+
     puntos: {
         type: Number,
-        required: [true, "Name is required!"],
+        required: [true, "La cantidad de puntos es requerida!"],
         max: [100000, "El máximo de puntos es 100,000!"],
-        min: [1000, "El mínimo de carácteres es 1,000!"],
+        min: [1000, "El mínimo de puntos es 1,000!"],
         default: 1000
     },
 
