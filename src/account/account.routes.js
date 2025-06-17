97f01b220e5f6ef6f613cd1b11d8bd90c0a168cd
pablocastillo4821@gmail.com <pablocastillo4821@gmail.com>
2025-06-16 22:27:14 -0600

    16/06/2025.

--- src/account/account.routes.js
+++ src/account/account.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { crearCuenta, obtenerCuentasPorUsuario, obtenerTodasCuentas, aprobarCuenta, deleteAccount} from "./account.controller.js";
+import { crearCuenta, obtenerCuentasPorUsuario, obtenerTodasCuentas, aprobarCuenta, deleteAccount, getOpciones} from "./account.controller.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
 router.post("/", validarJWT, crearCuenta);
-router.get("/", validarJWT, obtenerCuentasPorUsuario);
+router.get("/usuario", validarJWT, obtenerCuentasPorUsuario);
 router.get("/todas", validarJWT, obtenerTodasCuentas);
+router.get("/opciones", getOpciones);
 router.put("/:numeroCuenta/aprobar", validarJWT, aprobarCuenta);
 router.delete("/:id", validarJWT, deleteAccount);
 export default router;
