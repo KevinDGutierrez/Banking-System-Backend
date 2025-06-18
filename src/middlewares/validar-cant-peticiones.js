0391b2af83482a963a2621950dd19c428f7d676f
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-06-18 15:53:03 -0600

    Solicitudes ampliado

--- src/middlewares/validar-cant-peticiones.js
+++ src/middlewares/validar-cant-peticiones.js
@@ -1,11 +1,11 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
+    windowMs: 5 * 60 * 1000,
+    max: 1000,
     message: {
         success: false,
-        msg: "Demasiadas peticiones desde esta IP, por favor intente después de 15 minutos"
+        msg: "Demasiadas peticiones desde esta IP, por favor intente después de 5 minutos"
     }
 })
 
