bf9a4146916c2c0a6281a5452114a948f4d28951
pablocastillo4821@gmail.com <pablocastillo4821@gmail.com>
2025-06-18 22:49:43 -0600

    1

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.post("/register", registerCliente)
 router.post("/login", login)
 router.put("/:id/aprobar", validarJWT, aprobarCliente);
-router.put("/:id", validarJWT, updateCliente);
+router.put("/", validarJWT, updateCliente);
 router.delete("/", validarJWT, deleteCliente);
 router.get("/clientes", validarJWT, getClientesByAdmin);
 router.put("/cuentas/:numeroCuenta/tipo/", validarJWT, establecerTipoCuenta);
