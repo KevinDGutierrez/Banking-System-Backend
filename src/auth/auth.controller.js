6f0e2160397f8c9cb477a4a9e411732acaf63690
PabloCastillo10 <pablocastillo4821@gmail.com>
2025-07-01 08:57:11 -0600

    cambios

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
             userDetails: {
                 username: user.username,
                 token: token,
-                role: token.role
+                role: user.token.role
             }
         });
     } catch (error) {
