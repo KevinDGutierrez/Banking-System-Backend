306b68365027b51b85b3bef1b03e00ac85f382c1
JoaquinFigueroa5 <kinchin.figueroa@gmail.com>
2025-07-01 11:40:00 -0600

    Cambios

--- src/credito/credito.controller.js
+++ src/credito/credito.controller.js
@@ -4,7 +4,7 @@ import Cuenta from "../account/account.model.js";
 import { request, response } from "express";
 import { obtenerTipoCambio } from "../utils/apiDivisa.js";
 import { cuentaActiva, existeCreditoById, montoAprovacion, montoSolicitud, plazoSolicitud, soloAdmin, soloClient, tipoMonedaPermitida, verificarAprovacion, verificarAprovacionDelete } from "../helpers/db-validator-creditos.js";
-import { emailCreditoAprovado } from "../utils/sendEmail.js";
+import { emailCreditoAprovado, emailCreditoNoAprovado } from "../utils/sendEmail.js";
 
 export const solicitarCredito = async (req, res) => {
     try {
@@ -64,7 +64,7 @@ export const getCreditos = async (req = request, res = response) => {
         const userId = req.user._id;
         const esAdmin = req.user.role === 'ADMIN';
 
-        let query = { activo: true };
+        let query = { activo: false };
 
         if (esAdmin) {
             query.status = false;
@@ -181,6 +181,9 @@ export const deleteCredito = async (req, res = response) => {
         await verificarAprovacionDelete(credito);
 
         const creditoEliminado = await Credito.findByIdAndUpdate(id, { activo: false }, { new: true });
+
+        const user = await User.findById(credito.user);
+        await emailCreditoNoAprovado(user.correo, user.name, credito.moneda);
 
         res.status(200).json({
             success: true,
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -59,6 +59,35 @@ export const emailCreditoAprovado = async (to, nombreCliente, montoAprobado, mon
     }
 }
 
+export const emailCreditoNoAprovado = async (to, nombreCliente, moneda) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "bancoinnova86@gmail.com",
+                pass: "hmeb vaml fmto yxam"
+            }
+        })
+
+        const mailOptions = {
+            from: '"Banking System" <bancoinnova86@gmail.com>',
+            to,
+            subject: "Banking System - Aprobación de Crédito",
+            text: `Hola ${nombreCliente},\n\nGracias por tu solicitud de crédito. Te informamos que el monto mínimo que puedes solicitar es de ${moneda}1,000. Además, si solicitas un crédito de esta cantidad, el plazo máximo disponible será de 12 meses.\n\nSi deseas solicitar una cantidad superior, podemos ofrecerte plazos más largos. \n\nNo dudes en ponerte en contacto con nosotros si tienes alguna pregunta o si deseas más información sobre los términos disponibles.\n\nAtentamente,\nBanking System`
+        }
+
+        console.log(`Enviando correo a ${to}`);
+        await transporter.sendMail(mailOptions);
+        console.log("Correo enviado exitosamente");
+
+    } catch (error) {
+        console.error("Error al enviar el correo:", error.message);
+        throw new Error("No se pudo enviar el correo de no aprobación de crédito");
+    }
+}
+
 export const cambioDeDatos = async (to, nombreCliente, datos) => {
     try {
         const transporter = nodemailer.createTransport({
